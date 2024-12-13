c9c523463a0298e5db75f31088881be5503bce3c
tosiaki12 <45889262+kanawa12@users.noreply.github.com>
2024-12-13 23:19:17 +0900

    fixTimer

--- 2024KASAcode_py/mainfile/main.py
+++ 2024KASAcode_py/mainfile/main.py
@@ -9,13 +9,28 @@ from lib_lsm9ds1 import LSM9DS1
 
 print('start')
 
+timercount = 0
+isFlag1=False
+
+# コマンドラインに文字を出力する関数
+def timer_run(t):
+    global timercount, isFlag1 #グローバル変数にアクセスするためglobal キーワードで明示
+    timercount =  timercount + 1
+    if timercount % 300 == 0:
+        isFlag1=True
+    #print("acc",lsm.read_accel(),", tmp",bme.values_float[0])
+    return
+
 
 def timerinit():
     micropython.alloc_emergency_exception_buf(100)# 割り込み処理中の例外を作成するための設定
-    timer = Timer()# タイマーを作成
+    timer_base = Timer()# タイマーを作成
     # タイマーを初期化して、周期的にコマンドラインに文字を出力する
-    timer.init(mode=Timer.PERIODIC, freq=1, callback=timer_run)
-    return timer
+    timer_base.init(mode=Timer.PERIODIC, freq=100, callback=timer_run)
+    #タイマーが同時に作動した場合の挙動がよくわからないため一旦タイマーは1つだけ、そこまで時間が重要でない処理はフラグとIF文で
+    #呼び出した関数内の処理はできるだけ短く
+    return timer_base
+
 
 def seninit():
     i2c = machine.I2C(id=0, sda=machine.Pin(4), scl=machine.Pin(5))
@@ -25,20 +40,29 @@ def seninit():
     return i2c,bme,lsm
 
 
-# コマンドラインに文字を出力する関数
-def timer_run(timer):
-    print("Timer interrupt triggered")
-    print(bme.values_float[0])
-    print("gyr",lsm.read_gyro())
-    print("acc",lsm.read_accel())
-    print("mag",lsm.read_magnet())
-
-
-
-
-timer = timerinit()
-print("timer_start")
-i2c,bme = seninit()
-print('init_fin')
+print("begin_init")
+line = sys.stdin.readline()
+if not (line.startswith("run")):
+    print("stop code exit")
+    sys.exit()
+i2c,bme,lsm = seninit()
+timer_base= timerinit()
+print("start_timer")
+print('end_init')
 print('run')
+
+
+while True:
+    if isFlag1==True:#タイマ代わり
+        print("tt",timercount)
+        print(bme.values)
+        print("gyr",lsm.read_gyro())
+        print("acc",lsm.read_accel())
+        print("mag",lsm.read_magnet())
+        isFlag1=False
+        #
+    #
+#
+
+        
 
