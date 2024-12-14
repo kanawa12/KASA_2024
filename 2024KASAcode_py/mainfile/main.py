7f8c8feb5e3654a483f5c0d14ffb2ed141de5d1c
tosiaki12 <45889262+kanawa12@users.noreply.github.com>
2024-12-14 09:47:06 +0900

    fixTimerAndSerial

--- 2024KASAcode_py/mainfile/main.py
+++ 2024KASAcode_py/mainfile/main.py
@@ -1,8 +1,8 @@
 
-import machine
-from machine import Timer
+from machine import Pin,UART,Timer
 import micropython
 import sys
+import time
 
 import lib_bme280_float as bme280
 from lib_lsm9ds1 import LSM9DS1
@@ -14,19 +14,27 @@ isFlag1=False
 
 # コマンドラインに文字を出力する関数
 def timer_run(t):
-    global timercount, isFlag1 #グローバル変数にアクセスするためglobal キーワードで明示
+    global timercount, isFlag1,bmedata,accdata,gyrdata,magdata #グローバル変数にアクセスするためglobal キーワードで明示
     timercount =  timercount + 1
-    if timercount % 300 == 0:
+    start = time.ticks_ms() # ミリ秒カウンター値を取得
+    bmedata = bme.values
+    accdata = lsm.read_accel()
+    gyrdata = lsm.read_gyro()
+    magdata = lsm.read_magnet()
+    delta = time.ticks_diff(time.ticks_ms(), start) # 時差を計算
+    
+    if timercount % 10 == 0:
+        print("delta:",delta)
         isFlag1=True
     #print("acc",lsm.read_accel(),", tmp",bme.values_float[0])
     return
 
 
 def timerinit():
-    micropython.alloc_emergency_exception_buf(100)# 割り込み処理中の例外を作成するための設定
+    micropython.alloc_emergency_exception_buf(50)# 割り込み処理中の例外を作成するための設定
     timer_base = Timer()# タイマーを作成
     # タイマーを初期化して、周期的にコマンドラインに文字を出力する
-    timer_base.init(mode=Timer.PERIODIC, freq=100, callback=timer_run)
+    timer_base.init(mode=Timer.PERIODIC, freq=10, callback=timer_run)
     #タイマーが同時に作動した場合の挙動がよくわからないため一旦タイマーは1つだけ、そこまで時間が重要でない処理はフラグとIF文で
     #呼び出した関数内の処理はできるだけ短く
     return timer_base
@@ -40,29 +48,42 @@ def seninit():
     return i2c,bme,lsm
 
 
+bmedata = 0
+lsmdata = 0
+
 print("begin_init")
+chkpin = Pin(25,machine.Pin.OUT)
+chkpin.value(1)
+"""
 line = sys.stdin.readline()
 if not (line.startswith("run")):
     print("stop code exit")
     sys.exit()
+"""
 i2c,bme,lsm = seninit()
 timer_base= timerinit()
 print("start_timer")
+uart1 = UART(1, baudrate=115200,tx=Pin(8),rx=Pin(9))
 print('end_init')
+chkpin.value(0)
 print('run')
 
 
 while True:
     if isFlag1==True:#タイマ代わり
+        uart1.write("count:"+str(timercount))
         print("tt",timercount)
-        print(bme.values)
-        print("gyr",lsm.read_gyro())
-        print("acc",lsm.read_accel())
-        print("mag",lsm.read_magnet())
+        print(str(bmedata))
+        print(accdata,"\n",gyrdata,"\n",magdata)
         isFlag1=False
-        #
-    #
+
+    data = uart1.read()
+    if data != None:
+        chkpin.toggle()#RP2限定?
+        if data.find('temp')>=0:
+            uart1.write(bmedata)
+        elif data.find('acc')>=0:
+            uart1.write("accd:",lsm.read_accel())
+        else:
+            uart1.write("notcommand")
 #
-
-        
-
